00f974de64f55458dbe7f201c6d5fb5f2fe7dcdb
Stevan <stevans93@gmail.com>
2023-10-04 01:38:41 +0200

    Logout and SideBar Scroll

--- src/components/DashboardComponents/DashboardNavbar/DashboardNavbar.jsx
+++ src/components/DashboardComponents/DashboardNavbar/DashboardNavbar.jsx
@@ -1,16 +1,22 @@
-import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { BsEye } from "react-icons/bs";
 import { BiSolidBellRing } from "react-icons/bi";
 import { IoIosArrowDown } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 function DashboardNavbar({ handleOpen }) {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate("/");
+  }
+
   return (
     <nav className="flex justify-between h-[7vh] items-center relative w-[100%] flex right-0 px-4 shadow-lg">
       <div className="flex gap-5 items-center h-[5vh]">
-          <GiHamburgerMenu className="text-2xl text-black" onClick={handleOpen} />
+          <GiHamburgerMenu className="text-2xl text-black cursor-pointer" onClick={handleOpen} />
         <span className="w-px h-4/5 bg-gray-500 opacity-20"></span>
-        <button className="flex h-8 items-center px-5 bg-primary text-white rounded-lg gap-2">
+        <button className="flex h-8 items-center px-5 bg-primary text-white rounded-lg gap-2" onClick={handleBack}>
           <BsEye size="1.1rem" /> View page
         </button>
       </div>

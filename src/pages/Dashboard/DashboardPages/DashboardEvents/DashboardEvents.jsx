7f094f47d58bdc1e3c9620953be34bdebc1f43a3
emirlemes <emir.lemes@gmail.com>
2023-11-16 22:53:09 +0100

    event reload after delete

--- src/pages/Dashboard/DashboardPages/DashboardEvents/DashboardEvents.jsx
+++ src/pages/Dashboard/DashboardPages/DashboardEvents/DashboardEvents.jsx
@@ -1,113 +1,115 @@
-import React from "react";
-import TablePagination from "../../../../components/TablePagination/TablePagination";
-import { useState, useEffect } from "react";
-import EventService from "../../../../services/eventsService";
-import EventHeader from "../../../../components/DashboardComponents/DashboardHeaders/EventHeader/EventHeader";
-import DashboardEventsTable from "../../../../components/DashboardComponents/DashboardTables/DashboardEventsTable/DashboardEventsTable";
-import AddEventForm from "../../../../components/DashboardComponents/DashboardForms/AddEventForm/AddEventForm";
-import EditEventForm from "../../../../components/DashboardComponents/DashboardForms/EditEventForm/EditEventForm";
+import {useEffect, useState} from 'react'
+
+import AddEventForm from '../../../../components/DashboardComponents/DashboardForms/AddEventForm/AddEventForm'
+import DashboardEventsTable from '../../../../components/DashboardComponents/DashboardTables/DashboardEventsTable/DashboardEventsTable'
+import EditEventForm from '../../../../components/DashboardComponents/DashboardForms/EditEventForm/EditEventForm'
+import EventHeader from '../../../../components/DashboardComponents/DashboardHeaders/EventHeader/EventHeader'
+import EventService from '../../../../services/eventsService'
+import React from 'react'
+import TablePagination from '../../../../components/TablePagination/TablePagination'
 
 const DashboardEvents = () => {
-  const pageSizeOptions = [15, 30, 45];
-  const [numberOfPages, setNumberOfPages] = useState(1);
-  const [numberOfEvents, setNumberOfEvents] = useState();
-  const [events, setEvents] = useState(null);
+  const pageSizeOptions = [15, 30, 45]
+  const [numberOfPages, setNumberOfPages] = useState(1)
+  const [numberOfEvents, setNumberOfEvents] = useState()
+  const [events, setEvents] = useState(null)
 
-  const ncUser = JSON.parse(localStorage.getItem("nc_user"));
-  const clubId = ncUser ? ncUser.clubId : undefined;
+  const ncUser = JSON.parse(localStorage.getItem('nc_user'))
+  const clubId = ncUser ? ncUser.clubId : undefined
 
   const [selectedParams, setSelectedParams] = useState({
     pageNumber: 1,
     pageSize: pageSizeOptions[0],
-    clubId: clubId,
-  });
+    clubId: clubId
+  })
 
-  const [isAddEventModalOpen, setIsAddEventModalOpen] = useState(false);
-  const [isEditEventModalOpen, setIsEditEventModalOpen] = useState(false);
-  const [eventToEdit, setEventToEdit] = useState(null);
+  const [isAddEventModalOpen, setIsAddEventModalOpen] = useState(false)
+  const [isEditEventModalOpen, setIsEditEventModalOpen] = useState(false)
+  const [eventToEdit, setEventToEdit] = useState(null)
 
   const handleEditModalOpen = async (id) => {
-    await fetchEventById(id);
-    setIsEditEventModalOpen(true);
-  };
+    await fetchEventById(id)
+    setIsEditEventModalOpen(true)
+  }
 
   const handleEditModalClose = () => {
-    setIsEditEventModalOpen(false);
-  };
+    setIsEditEventModalOpen(false)
+  }
 
   const handleEventModalOpen = () => {
-    setIsAddEventModalOpen(true);
-  };
+    setIsAddEventModalOpen(true)
+  }
 
   const handleEventModalClose = () => {
-    setIsAddEventModalOpen(false);
-  };
+    setIsAddEventModalOpen(false)
+  }
 
   const handlePageSizeChange = (value) => {
     setSelectedParams((selectedParams) => ({
       ...selectedParams,
       pageSize: value,
-      pageNumber: 1,
-    }));
-  };
+      pageNumber: 1
+    }))
+  }
 
   const handleNextPage = () => {
     setSelectedParams((selectedParams) => ({
       ...selectedParams,
-      pageNumber: selectedParams.pageNumber + 1,
-    }));
-  };
+      pageNumber: selectedParams.pageNumber + 1
+    }))
+  }
 
   const handlePreviousPage = () => {
     setSelectedParams((selectedParams) => ({
       ...selectedParams,
-      pageNumber: selectedParams.pageNumber - 1,
-    }));
-  };
+      pageNumber: selectedParams.pageNumber - 1
+    }))
+  }
 
   const handleDelete = async (id) => {
-    await fetchDeleteEventById(id);
-  };
+    await fetchDeleteEventById(id)
+    setEvents(events.filter((e) => e._id !== id))
+  }
 
   const fetchEventById = async (id) => {
     try {
-      const response = await EventService.getSingleEvent(id);
+      const response = await EventService.getSingleEvent(id)
 
       if (response) {
         // Handle success
-        setEventToEdit(response); // Assuming setEventToEdit is a state updater function
+        setEventToEdit(response) // Assuming setEventToEdit is a state updater function
 
-        console.log("Event fetched successfully");
+        console.log('Event fetched successfully')
         // You can perform additional actions if needed
       } else {
         // Handle failure
-        console.log("Failed to fetch event");
+        console.log('Failed to fetch event')
         // You can perform additional actions if needed
       }
     } catch (error) {
       // Handle any errors here
-      console.error("An error occurred while fetching the event:", error);
+      console.error('An error occurred while fetching the event:', error)
     }
-  };
+  }
 
   const fetchDeleteEventById = async (id) => {
     try {
-      const response = await EventService.deleteEvent(id);
+      const response = await EventService.deleteEvent(id)
 
       if (response) {
         // Handle success
-        console.log("Event deleted successfully");
+        console.log('Event deleted successfully')
         // You can perform additional actions if needed
       } else {
         // Handle failure
-        console.error("Failed to delete event");
+        console.error('Failed to delete event')
         // You can perform additional actions if needed
       }
     } catch (error) {
       // Handle any errors here
-      console.error("An error occurred while deleting the event:", error);
+      console.error('An error occurred while deleting the event:', error)
     }
-  };
+  }
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -117,21 +119,20 @@ const DashboardEvents = () => {
           selectedParams.pageSize,
           selectedParams.clubId
           // Include other parameters as needed
-        );
+        )
 
         if (eventsData) {
-          setEvents(eventsData.events);
-          setNumberOfPages(eventsData.numberOfPages);
-          setNumberOfEvents(eventsData.numberOfEvents);
+          setEvents(eventsData.events)
+          setNumberOfPages(eventsData.numberOfPages)
+          setNumberOfEvents(eventsData.numberOfEvents)
         }
       } catch (error) {
         // Handle any errors here
-        console.error("An error occurred while fetching events:", error);
+        console.error('An error occurred while fetching events:', error)
       }
-    };
-
-    fetchEvents();
-  }, [selectedParams]);
+    }
+    fetchEvents()
+  }, [selectedParams])
 
   return (
     <div className="bg-[#F9F9F9] pt-5 h-full px-3 py-10 shadow-lg">
@@ -161,10 +162,7 @@ const DashboardEvents = () => {
         selectedParams={selectedParams}
       />
 
-      <AddEventForm
-        isAddEventModalOpen={isAddEventModalOpen}
-        handleEventModalClose={handleEventModalClose}
-      />
+      <AddEventForm isAddEventModalOpen={isAddEventModalOpen} handleEventModalClose={handleEventModalClose} />
 
       {eventToEdit && (
         <EditEventForm
@@ -174,7 +172,7 @@ const DashboardEvents = () => {
         />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default DashboardEvents;
+export default DashboardEvents
